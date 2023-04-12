1354adb18867fdd11951b7156687e4a8bd5f185e
slee1996 <spencerlee96@gmail.com>
2023-04-12 17:30:05 -0600

    feat: slightly improved error handling

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ const router = new Router();
 app.use(cors());
 app.use(bodyParser());
 
+// error handling middleware
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    console.error(err);
+    ctx.status = err.status || 500;
+    ctx.body = { error: err.message };
+    ctx.app.emit("error", err, ctx);
+  }
+});
+
 // logger
 app.use(async (ctx, next) => {
   await next();
@@ -44,11 +56,11 @@ async function promptGpt(storyPrompt) {
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      n: 2,
+      n: 5,
       messages: [
         {
           role: "system",
-          content: `You are a software engineer, your job is to help me generate new functionality, and refactor code that is fed to you. The language you are writing in is javascript. Do not include any example usages in the same code block you provide the code in. This is your prompt:`,
+          content: `You are a software engineer, your job is to help me generate new functionality, and refactor code that is fed to you. The language you are writing in is javascript. Do not include any example usages in the same code block you provide the code in. Provide only the code in your response. This is your prompt:`,
         },
         ...storyPrompt,
       ],
@@ -65,8 +77,9 @@ router.post("/", async (ctx) => {
   try {
     const chats = await promptGpt(ctx.request.body.prompt);
     const generatedFunctions = new Set();
+    const codeExecutionsWereSuccessful = [];
 
-    chats.slice(0, 9).forEach(async (chat, i) => {
+    chats.forEach(async (chat, i) => {
       const extractedCode = extractCodeFromString(chat.message.content);
       const functionName = getFunctionName(extractedCode);
 
@@ -76,37 +89,50 @@ router.post("/", async (ctx) => {
 
       console.log(`Start running ${functionName}-${i} in isolated vm-${i}`);
 
-      await runCodeInIsolatedVm(extractedCode).catch((err) => {
-        throw err;
-      });
+      let codeExecutionSuccessful = false;
+
+      runCodeInIsolatedVm(extractedCode)
+        .then((res) => {
+          codeExecutionSuccessful = true;
+        })
+        .catch((err) => {
+          console.error(err);
+          ctx.body = err;
+        });
+
       const ms = Date.now() - start;
 
       console.log(
         `End running ${functionName}-${i} in isolated vm-${i}`,
+        `Success: ${codeExecutionSuccessful}`,
         ms + "ms"
       );
 
-      const functionCodeWithExport = `${extractedCode}\nmodule.exports = { ${
-        extractedCode.match(/function\s+(\w+)/)[1]
-      } };`;
+      if (codeExecutionSuccessful) {
+        const functionCodeWithExport = `${extractedCode}\nmodule.exports = { ${
+          extractedCode.match(/function\s+(\w+)/)[1]
+        } };`;
 
-      fs.writeFileSync(
-        `${functionName}${i}.js`,
-        functionCodeWithExport,
-        "utf8"
-      );
-      generateDynamicTestFile(
-        `${functionName}${i}.test.js`,
-        functionName,
-        extractedCode
-      );
+        fs.writeFileSync(
+          `${functionName}${i}.js`,
+          functionCodeWithExport,
+          "utf8"
+        );
+        generateDynamicTestFile(
+          `${functionName}${i}.test.js`,
+          functionName,
+          extractedCode
+        );
+        codeExecutionsWereSuccessful.push(codeExecutionSuccessful);
+      }
     });
+    console.log(codeExecutionsWereSuccessful);
 
-    await runJestTests();
-
-    ctx.body = chats;
+    if (codeExecutionsWereSuccessful.every((success) => success === true)) {
+      await runJestTests();
+      ctx.body = chats;
+    }
   } catch (err) {
-    console.error(err);
     ctx.body = err;
   }
 });
